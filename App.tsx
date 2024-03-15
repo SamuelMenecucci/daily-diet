93e8699abca062aec44cd769e3a2169a3ecd40c2
Samuel Menecucci Moura <samuelmm@ciandt.com>
2024-03-15 08:01:35 -0300

    feat: navigation config

--- App.tsx
+++ App.tsx
@@ -6,18 +6,18 @@ import {
 } from "@expo-google-fonts/nunito-sans";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/theme";
-import { Home } from "@screens/Home";
 import { Loading } from "@components/Loading";
+import { Routes } from "@routes/index";
 
 export default function App() {
-  const [fontsLoaded, setFontsLoaded] = useFonts({
+  const [fontsLoaded] = useFonts({
     NunitoSans_400Regular,
     NunitoSans_700Bold,
   });
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Home /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
